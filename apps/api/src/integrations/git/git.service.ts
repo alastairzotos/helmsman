6c7e393b36545a5b7ea6212c1d67e7a5845806f5
Alastair Zotos <alastairzotos@gmail.com>
2023-06-02 12:14:22 +0300

    start taking password

--- apps/api/src/integrations/git/git.service.ts
+++ apps/api/src/integrations/git/git.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { clone, getRemoteInfo } from 'isomorphic-git';
+import * as git from 'isomorphic-git';
 import http from "isomorphic-git/http/node";
 import * as fs from 'fs';
 import * as path from 'path';
@@ -9,19 +9,22 @@ import { rimraf } from 'rimraf';
 @Injectable()
 export class GitService {
   async getRemoteInfo(url: string) {
-    return await getRemoteInfo({
+    return await git.getRemoteInfo({
       url,
       http
     })
   }
   
-  async clone(url: string, onProgress: (phrase: string, progress: number) => void) {
+  async clone(repoUrl: string, onProgress: (phrase: string, progress: number) => void) {
+    const url = new URL(repoUrl);
+    url.password = "testing";
+
     const projectName = uuid();
 
-    await clone({
+    await git.clone({
       fs,
       http,
-      url,
+      url: url.toString(),
       dir: path.resolve(this.getRootPath(), projectName),
       onProgress: (e) => {
         const percent = (e.loaded / e.total) * 100;
