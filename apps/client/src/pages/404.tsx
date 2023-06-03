391945b5ca471a6ff3b00ab9a4760d413bd7bb90
Alastair Zotos <alastairzotos@gmail.com>
2023-06-03 22:57:06 +0300

    feat: add 404 and 500 pages

---
+++ apps/client/src/pages/404.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "antd"
+
+const { Title } = Typography;
+
+export default function Page404() {
+  return <Title>Not found</Title>
+}
---
+++ apps/client/src/pages/500.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "antd"
+
+const { Title } = Typography;
+
+export default function Page500() {
+  return <Title>Internal server error</Title>
+}
