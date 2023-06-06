3e72f7588c6a7162ff3ccce18bc92e2637c538e0
Alastair Zotos <alastairzotos@gmail.com>
2023-06-06 21:55:19 +0300

    remove login form

--- apps/client/src/pages/login/index.tsx
+++ apps/client/src/pages/login/index.tsx
@@ -1,8 +1,20 @@
-import { LoginForm } from "@/components/auth/login";
+import { getEnv } from "@/utils/env";
+import { Button } from "antd";
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 
 const LoginPage: NextPage = () => {
-  return <LoginForm />;
+  const router = useRouter();
+
+  const handleLoginClick = async () => {
+    router.push(`${getEnv().idServerUrl}/login?propertyId=bitmetro.mission-control&fwd=${encodeURIComponent(router.query.fwd as string)}`);
+  }
+
+  return (
+    <Button onClick={handleLoginClick}>
+      Login
+    </Button>
+  )
 }
 
 LoginPage.getInitialProps = () => ({});
