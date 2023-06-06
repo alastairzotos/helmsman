a624f08c7f475252911614fa23373f4a2d33e902
Alastair Zotos <alastairzotos@gmail.com>
2023-06-06 22:22:19 +0300

    don't create id property

--- apps/api/src/integrations/identity/identity.service.ts
+++ apps/api/src/integrations/identity/identity.service.ts
@@ -1,41 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { IdentityProvider } from "@bitmetro/auth-node";
 import { EnvironmentService } from "environment/environment.service";
-import fetch from "node-fetch";
 
 @Injectable()
 export class IdentityService {
   private identityProvider: IdentityProvider;
 
-  constructor(
-    private readonly env: EnvironmentService,
-  ) {
+  constructor(env: EnvironmentService) {
     this.identityProvider = new IdentityProvider({
       idServer: env.get().idServer,
     })
-  }
-
-  async getProperty() {
-    const res = await fetch(`${this.env.get().idServer}/api/v1/properties`, {
-      method: 'PUT',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name: "Mission Control",
-        uniqueId: "bitmetro.mission-control",
-        userDetails: [],
-        defaultUserData: {
-          role: "user",
-        },
-        loginModes: ["email_and_password"],
-        forwardUrl: this.env.get().appUrl,
-        logo: "https://i.imgur.com/QkVl0LB.png"
-      })
-    })
-
-    return await res.json();
   }
 
   async verifyIdentity(accessToken: string) {
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RequestMethod } from '@nestjs/common';
-import { IdentityService } from 'integrations/identity/identity.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,8 +10,6 @@ async function bootstrap() {
   });
 
   app.enableCors();
-
-  console.log(await app.get<IdentityService>(IdentityService).getProperty());
 
   await app.listen(4001);
 }
