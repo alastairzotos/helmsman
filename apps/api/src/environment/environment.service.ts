a9e5f32547c604787e777bfa3e7a2bcf49de2225
Alastair Zotos <alastairzotos@gmail.com>
2023-06-08 18:06:01 +0300

    remove unused env vars

--- apps/api/src/environment/environment.service.ts
+++ apps/api/src/environment/environment.service.ts
@@ -5,9 +5,7 @@ export class EnvironmentService {
   private readonly env_vars = {
     dbConnectionString: process.env.DB_CONNECTION_STRING as string,
     aesKey: process.env.AES_KEY as string,
-    jwtSigningKey: process.env.JWT_SIGNING_SECRET as string,
     idServerApiKey: process.env.ID_SERVER_API_KEY as string,
-    appUrl: process.env.APP_URL as string,
   };
 
   get() {
