5f4432d9eeab810caee92ac39281d147f6e28484
Alastair Zotos <alastairzotos@gmail.com>
2023-06-13 17:17:31 +0300

    browse by namespace

--- apps/client/src/components/projects/projects-list.tsx
+++ apps/client/src/components/projects/projects-list.tsx
@@ -1,33 +1,63 @@
 import { StatusSwitch } from "@/components/_core/status-switch";
 import { useGetAllProjects } from "@/state/projects.state";
 import { urls } from "@/urls";
-import { Button, List, Space } from "antd";
+import { Button, Col, Menu, Row, Space } from "antd";
 import { PlusOutlined } from '@ant-design/icons';
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { IProjectDto, WithId } from "models";
+import { createNavMenuItem } from "@bitmetro/app-layout-antd";
+import { useRouter } from "next/router";
+
+type NamespacesWithProjects = Record<string, WithId<IProjectDto>[]>;
 
 export const ProjectsList: React.FC = () => {
+  const router = useRouter();
+
   const [loadStatus, loadProjects, projects] = useGetAllProjects(s => [s.status, s.request, s.value]);
+  const [selectedNs, setSelectedNs] = useState<string | null>(null);
 
   useEffect(() => {
     loadProjects();
   }, []);
 
+  const namespacesWithProjects: NamespacesWithProjects = (projects || []).reduce(
+    (acc, cur) => ({
+      ...acc,
+      [cur.namespace]: [
+        ...(acc[cur.namespace] || []),
+        cur
+      ]
+    }),
+    {} as NamespacesWithProjects,
+  )
+
   return (
     <StatusSwitch status={loadStatus}>
       <Space direction="vertical" style={{ width: 400 }}>
-        {projects && (
-          <List
-            dataSource={projects}
-            renderItem={project => (
-              <Link href={urls.projects.project(project._id)}>
-                <List.Item>
-                  {project.name}
-                </List.Item>
-              </Link>
-            )}
-          />
-        )}
+        <Row>
+          <Col span={12}>
+            <Menu
+              items={
+                Object.keys(namespacesWithProjects).map((ns) => (
+                  createNavMenuItem(ns, ns)
+                ))
+              }
+              onSelect={(e) => setSelectedNs(e.key)}
+            />
+          </Col>
+
+          {!!selectedNs && (
+            <Col span={12}>
+              <Menu
+                items={namespacesWithProjects[selectedNs].map((proj) => (
+                  createNavMenuItem(proj.name, proj._id)
+                ))}
+                onSelect={(e) => router.push(urls.projects.project(e.key))}
+              />
+            </Col>
+          )}
+        </Row>
 
         <Link href={urls.projects.create()}>
           <Button type="dashed" block icon={<PlusOutlined />}>
